3ce87b76b06eb0533ca9b2cbc6a638479bf96d81
cryoxero <cryoxero@proton.me>
2025-06-09 19:27:04 +0330

    Add longest line

--- src/wc_python/__main__.py
+++ src/wc_python/__main__.py
@@ -8,6 +8,7 @@ class States(Enum):
     CHARS = 2
     LINES = 4
     WORDS = 8
+    LONGEST = 16
 
 
 def get_state(args):
@@ -17,6 +18,7 @@ def get_state(args):
     state |= int(args.words) * States.WORDS.value
     state |= int(args.bytes) * States.BYTES.value
     state |= int(args.chars) * States.CHARS.value
+    state |= int(args.max_line_length) * States.LONGEST.value
     
     return state or States.LINES.value | States.WORDS.value | States.BYTES.value
 
@@ -30,6 +32,7 @@ def get_file_details(path):
         "chars": 0,
         "lines": 0,
         "words": 0,
+        "longest": 0,
     }
     if not exists(path):
         details["exists"] = False
@@ -44,11 +47,12 @@ def get_file_details(path):
         details["bytes"] = getsize(path)
         with open(path, "r") as file:
             for line in file:
+                line_length = 0
                 # LINES
                 details["lines"] += 1
                 for char in line:
                     # CHARS
-                    details["chars"] += 1
+                    line_length += 1
                     # WORDS
                     if char.isspace():
                         if is_word:
@@ -56,6 +60,14 @@ def get_file_details(path):
                         is_word = False
                     else:
                         is_word = True
+                
+                # LONGEST_LINE
+                if line_length > details["longest"]:
+                    details["longest"] = line_length - int(line[-1] == "\n")
+                
+                # TOTAL CHARS
+                details["chars"] += line_length;
+            
             # LAST WORD CHECK
             if is_word:
                 details["words"] += 1
@@ -73,17 +85,22 @@ def get_stdin_details():
         "chars": 0,
         "words": 0,
         "lines": 0,
+        "longest": 0,
     }
 
     try:
         is_word = False
+        line_length = 0
         for char in stdin.read():
             if char.isspace():
                 # LINES
                 if is_word:
                     details["words"] += 1
                 if char == "\n":
+                    if line_length > details["longest"]:
+                        details["longest"] = line_length - 1
                     details["lines"] += 1
+                    line_length = 0
                 is_word = False
             else:
                 is_word = True
@@ -92,9 +109,13 @@ def get_stdin_details():
             details["bytes"] += len(char.encode("utf-8"))
             # CHARS
             details["chars"] += 1
+            line_length += 1
         # LAST WORD CHECK
         if is_word:
             details["words"] += 1
+
+        if line_length > details["longest"]:
+            details["longest"] = line_length
     
     except Exception:
         details["error"] = True
@@ -111,6 +132,8 @@ def report(state, details, path=""):
         print(f'{details["bytes"]:3}', end="\t")
     if state & States.CHARS.value:
         print(f'{details["chars"]:3}', end="\t")
+    if state & States.LONGEST.value:
+        print(f'{details["longest"]:3}', end="\t")
     print(path)
 
 
@@ -120,8 +143,11 @@ def main():
     parser.add_argument("-m", "--chars", action="store_true", help="print the character counts")
     parser.add_argument("-l", "--lines", action="store_true", help="print the newline counts")
     parser.add_argument("-w", "--words", action="store_true", help="print the word counts")
+    parser.add_argument("-L", "--max-line-length", action="store_true", help="print the maximum display width")
     args = parser.parse_args()
     state = get_state(args)
+    details = get_stdin_details()
+    report(state, details)
     quit(0)
 
 if __name__ == "__main__":
